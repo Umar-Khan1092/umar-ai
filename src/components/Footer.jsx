4c09ec4e65a412f51d9005a933a992fd7f848b6f
Muhammad Umar Attique <umarmalik3120225@gmail.com>
2026-02-03 18:36:36 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-col md:flex-row justify-between items-center w-full">
                     <p className="text-sm text-center md:text-left text-slate-500">
-                        &copy; {new Date().getFullYear()} AI Consultant Umar Attique. All rights reserved.
+                        &copy;  AI Consultant Umar Attique. All rights reserved.
                     </p>
                 </div>
             </div>
